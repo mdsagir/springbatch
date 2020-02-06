a0fe4c2ab6b84d410846c793b6d90d21b5f7102a
msager <md.sager@bnedloudcloud.com>
2020-02-06 11:42:58 +0530

    added new csv to db

---
+++ csvtodb/src/main/java/com/springbatch/basicio/BasicioApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.basicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicioApplication.class, args);
+	}
+
+}

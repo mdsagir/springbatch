46455d85d688af956288d084afeb8548d61c08f5
mdsagir <tech.sagir@gmail.com>
2020-02-09 13:58:18 +0530

    added new file

---
+++ skippolicy/src/main/java/com/springbatch/multisteps/MultistepsApplication.java
@@ -0,0 +1,12 @@
+package com.springbatch.multisteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultistepsApplication.class, args);
+    }
+}
---
+++ skippolicy/src/main/java/com/springbatch/multisteps/config/BatchConfig.java
@@ -0,0 +1,105 @@
+package com.springbatch.multisteps.config;
+
+import com.springbatch.multisteps.model.Employee;
+import com.springbatch.multisteps.policy.JobSkipPolicy;
+import com.springbatch.multisteps.processor.EmployeProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+
+    @Autowired
+    private EmployeProcessor employeProcessor;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public FlatFileItemReader<Employee> employeeReader() throws Exception {
+        FlatFileItemReader<Employee> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new FileSystemResource("src/main/resources/employees.csv"));
+        flatFileItemReader.setName("CSV-Reader");
+      //  flatFileItemReader.setLinesToSkip(1); //To skip first line
+        flatFileItemReader.setLineMapper(lineMapper());
+        return flatFileItemReader;
+    }
+
+
+    @Bean
+    public JdbcBatchItemWriter<Employee> employeeWriter() {
+        JdbcBatchItemWriter<Employee> itemWriter = new JdbcBatchItemWriter<Employee>();
+        itemWriter.setDataSource(dataSource);
+        itemWriter.setSql("insert into employee (employeeId, firstName, lastName, email, age) values (:employeeId, :firstName, :lastName, :email, :age)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Employee>());
+        return itemWriter;
+    }
+
+    @Bean
+    public LineMapper<Employee> lineMapper() {
+        DefaultLineMapper<Employee> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("employeeId", "firstName", "lastName", "email", "age");
+
+        BeanWrapperFieldSetMapper<Employee> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Employee.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return defaultLineMapper;
+    }
+
+    @Bean
+    public Job job() throws Exception{
+        return jobBuilderFactory.get("myFirstJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(step())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step() throws Exception{
+        return stepBuilderFactory.get("firstSteps").<Employee,Employee>chunk(1)
+                .reader(employeeReader())
+                .processor(employeProcessor)
+                .writer(employeeWriter())
+                .faultTolerant()
+                .skipPolicy(jobSkipPolicy())
+                .build();
+    }
+
+    @Bean
+    public JobSkipPolicy jobSkipPolicy(){
+        return new JobSkipPolicy();
+    }
+}
